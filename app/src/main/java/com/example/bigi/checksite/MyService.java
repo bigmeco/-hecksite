d6e5ad2fefb6fa342055328327739b963c4d8c77
DESKTOP-PA6B369\bigi <bigmecoprog@gmail.com>
2017-12-31 17:24:53 +0300

    otkat

--- app/src/main/java/com/example/bigi/checksite/MyService.java
+++ app/src/main/java/com/example/bigi/checksite/MyService.java
@@ -1,48 +1,23 @@
 package com.example.bigi.checksite;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
 import android.os.IBinder;
-import android.widget.Toast;
 
-public class MyService extends BroadcastReceiver {
-    NotificationManager nm;
+public class MyService extends Service {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
-        Intent scheduledIntent = new Intent(context, MyService.class);
-        scheduledIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    public void onCreate() {
+        super.onCreate();
+    }
 
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                scheduledIntent, 0);
 
-        nm = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
+    public MyService() {
+    }
 
-        Resources res = context.getResources();
-        Notification.Builder builder = new Notification.Builder(context);
-
-        builder.setContentIntent(contentIntent)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setLargeIcon(
-                        BitmapFactory.decodeResource(res,
-                                R.drawable.ic_launcher_background))
-                .setTicker("Накорми кота!")
-                // текст в строке состояния
-                .setWhen(System.currentTimeMillis()).setAutoCancel(true)
-                .setContentTitle("Время кормить кота") // Заголовок уведомления
-                .setContentText("Да накорми кота, наконец"); // Текст
-        // уведомления
-        Notification n = builder.build();
-        System.out.println("fdgsdfgd");
-        nm.notify(1, n);
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
