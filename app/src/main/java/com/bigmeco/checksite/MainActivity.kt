3dd4c6303e1a65dc83dc502c6d5b153eeaa3f8e7
bigmeco <bigmecoprog@gmail.com>
2018-04-30 16:25:47 +0300

    start kotlin

--- app/src/main/java/com/bigmeco/checksite/MainActivity.kt
+++ app/src/main/java/com/bigmeco/checksite/MainActivity.kt
@@ -2,11 +2,18 @@ package com.bigmeco.checksite
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_lista)
+
+//        UrlListView.layoutManager = LinearLayoutManager(this)
+//        UrlListView.adapter = MyFilmAdapter(realm.where(MyFilm::class.java).findAll(), {
+//            topFilmList!!.adapter.notifyDataSetChanged()
+//        })
     }
 }
